1793eb888fcbd37b566f843bc567ee528b7849e2
Shakya Tilak <75114840+Tilak1028-st@users.noreply.github.com>
2022-06-16 00:12:21 -0700

    - design Instagram post UI.

---
+++ InstagramClone/Cell/FeedTableViewCell.swift
@@ -0,0 +1,38 @@
+//
+//  FeedTableViewCell.swift
+//  InstagramClone
+//
+//  Created by PCS213 on 15/06/22.
+//
+
+import UIKit
+
+class FeedTableViewCell: UITableViewCell {
+
+    weak var growingCellDelegate: GrowingCellProtocol?
+    
+    @IBOutlet weak var commentTextView: UITextView!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        commentTextView.delegate = self
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+    }
+    
+}
+
+extension FeedTableViewCell: UITextViewDelegate {
+    
+    func textViewDidChange(_ textView: UITextView) {
+        if let deletate = growingCellDelegate {
+            deletate.updateHeightOfRow(self, textView)
+        }
+    }
+}
+
+protocol GrowingCellProtocol: AnyObject {
+    func updateHeightOfRow(_ cell: FeedTableViewCell, _ textView: UITextView)
+}
--- InstagramClone/Constant/StringConstant.swift
+++ InstagramClone/Constant/StringConstant.swift
@@ -10,4 +10,6 @@ import Foundation
 class Constant
 {
     static var storyCollectionCell: String = "StoryCollectionViewCell"
+    static var feedTableViewCell: String = "FeedTableViewCell"
+    static var feedHeadercell: String = "FeedHeaderTableViewCell"
 }
