1a1db71d16b3a23e372eb0a069d9254a64e3248c
Shakya Tilak <75114840+Tilak1028-st@users.noreply.github.com>
2022-06-15 21:56:31 -0700

    - develop story feature for application.

---
+++ InstagramClone/Cell/StoryCollectionViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  StoryCollectionViewCell.swift
+//  InstagramClone
+//
+//  Created by PCS213 on 15/06/22.
+//
+
+import UIKit
+
+class StoryCollectionViewCell: UICollectionViewCell {
+
+    @IBOutlet weak var userNameLabel: UILabel!
+    @IBOutlet weak var storyDpImgView: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        self.storyDpImgView.layer.borderWidth = 1
+        self.storyDpImgView.layer.borderColor = UIColor.gray.cgColor
+        self.storyDpImgView.layer.cornerRadius = self.storyDpImgView.frame.size.width / 5
+        self.userNameLabel.text = "Username"
+    }
+
+}
--- InstagramClone/ViewController/HomeViewController.swift
+++ InstagramClone/ViewController/HomeViewController.swift
@@ -9,9 +9,14 @@ import UIKit
 
 class HomeViewController: UIViewController {
 
+    @IBOutlet weak var storyCollectionView: UICollectionView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         addLeftBarIcon(named: "Instagram_logo.svg")
+        
+        storyCollectionView.register(UINib.init(nibName: Constant.storyCollectionCell, bundle: nil), forCellWithReuseIdentifier: Constant.storyCollectionCell)
     }
     
     
@@ -27,6 +32,42 @@ class HomeViewController: UIViewController {
         heightConstraint.isActive = true
         widthConstraint.isActive = true
         navigationItem.leftBarButtonItem =  imageItem
+    }
+    
+}
+
+
+extension HomeViewController: UICollectionViewDelegate, UICollectionViewDataSource
+{
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        1
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        10
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let item = storyCollectionView.dequeueReusableCell(withReuseIdentifier: Constant.storyCollectionCell, for: indexPath) as! StoryCollectionViewCell
+        return item
+    }
+}
+
+extension HomeViewController: UICollectionViewDelegateFlowLayout
+{
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize
+    {
+        let collectionWidth = collectionView.bounds.width
+        let collectionHeight = collectionView.bounds.height
+        return CGSize(width: collectionWidth/4, height: collectionHeight)
+    }
+
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+        return 0
+    }
+
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return 0
     }
     
 }
