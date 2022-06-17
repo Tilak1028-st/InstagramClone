59ba91ca2242093690aa3570f99a3c6a94e15e05
Shakya Tilak <75114840+Tilak1028-st@users.noreply.github.com>
2022-06-17 06:21:45 -0700

    - profile screen segment feature

--- InstagramClone/ViewController/ProfileViewController.swift
+++ InstagramClone/ViewController/ProfileViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import ScrollableSegmentedControl
 
 class ProfileViewController: UIViewController {
 
@@ -13,6 +14,8 @@ class ProfileViewController: UIViewController {
     @IBOutlet weak var editProfileButton: UIButton!
     @IBOutlet weak var barButtonItem: UIBarButtonItem!
     @IBOutlet weak var profileimageView: UIView!
+    @IBOutlet weak var segment: ScrollableSegmentedControl!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -24,5 +27,29 @@ class ProfileViewController: UIViewController {
         editProfileButton.layer.borderColor = UIColor.gray.cgColor
         addfollowerButton.layer.borderWidth = 0.7
         addfollowerButton.layer.borderColor = UIColor.gray.cgColor
+        segemntConfiguration()
     }
+    
+    func segemntConfiguration()
+    {
+
+        segment.backgroundColor = UIColor(red: 237.0/255, green: 140.0/255, blue: 12.0/255, alpha: 1.0)
+        segment.segmentStyle = .textOnly
+        segment.insertSegment(withTitle: "post", at: 0)
+        segment.insertSegment(withTitle: "tag", at: 1)
+        segment.underlineSelected = true
+        segment.fixedSegmentWidth = false
+        segment.addTarget(self, action: #selector(segmentSelected(sender:)), for: .valueChanged)
+        
+        let normalFont = UIFont(name: "Helvetica", size: 17.0)
+        segment.setTitleTextAttributes([NSAttributedString.Key.font: normalFont as Any,NSAttributedString.Key.foregroundColor: UIColor.systemGray5], for: UIControl.State.normal)
+        segment.setTitleTextAttributes([NSAttributedString.Key.font: normalFont as Any,NSAttributedString.Key.foregroundColor: UIColor.white], for: .selected)
+    }
+    
+    @objc func segmentSelected(sender:ScrollableSegmentedControl) {
+        if sender.selectedSegmentIndex == 0 {
+            print("Hety")
+        }
+    }
+    
 }
