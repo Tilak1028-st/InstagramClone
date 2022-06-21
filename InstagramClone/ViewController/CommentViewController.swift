1e690cdfbe28cb501815c3e6aa80fd760e83c255
Shakya Tilak <75114840+Tilak1028-st@users.noreply.github.com>
2022-06-20 22:34:10 -0700

    - changed cell project structure

--- InstagramClone/ViewController/CommentViewController.swift
+++ InstagramClone/ViewController/CommentViewController.swift
@@ -9,9 +9,25 @@ import UIKit
 
 class CommentViewController: UIViewController {
 
+    @IBOutlet weak var commentTableView: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
 
     }
 
+}
+
+//MARK: - Extension for tableview delegate and datasource methods.
+
+extension CommentViewController: UITableViewDelegate, UITableViewDataSource
+{
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell
+    }
+    
+    
 }
